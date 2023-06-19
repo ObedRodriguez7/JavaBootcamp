9b3992c183c97eab580476afa26d37eb41d90fd4
Obed Rodriguez <obedgrodriguez7@gmail.com>
2023-06-19 17:58:30 -0600

    update class 8

--- modules/module4/Clase-8/PasswordGenerator.java
+++ modules/module4/Clase-8/PasswordGenerator.java
@@ -1,5 +1,22 @@
 public class PasswordGenerator {
     public static void main(String[] args) {
-        
+        String myPassword = getPassword();
+        System.out.println(myPassword);
+    }
+
+    public static String getPassword() {
+        StringBuilder str = new StringBuilder();
+        String[] capitalLetters = {"ZWX", "WRV", "WXY", "QML", "PEK", "TEC", "SWD", "PKD", "VNQ", "TAK"};
+        String[] letters = {"awokd", "eowmx", "xyz", "anhd", "efgh", "ijkl", "mnop", "qrst", "ksmcr", "xerfs"};
+        String[] numbers = {"739", "249", "135", "536", "789", "321", "654", "987", "301", "027"};
+        String[] characters = {"$%#", "@&!", "*^%", "#$@", "!@#", "%^*", "@#$", "&*%", "^#-", "&!$"};
+
+        int numberRandom = (int) (Math.random() * 9) + 1;
+        str.append(capitalLetters[numberRandom]);
+        str.append(letters[numberRandom]);
+        str.append(numbers[numberRandom]);
+        str.append(characters[numberRandom]);
+
+        return str.toString();
     }
 }
